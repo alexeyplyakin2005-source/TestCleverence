74c4dcc7047c2aed3e39d7661e733f6c937d23ff
Alexey <a.plyakin@sh.dunice.net>
2026-04-19 22:17:48 +0300

    Добавлен проект Server/ Задача #2

---
+++ Server/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Server;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var tasks = new Task[10];
+
+        // читатели
+        for (int i = 0; i < 5; i++)
+        {
+            tasks[i] = Task.Run(() =>
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine($"{StaticServer.GetCount()}");
+                    Thread.Sleep(50);
+                }
+            });
+        }
+
+        // писатели
+        for (int i = 5; i < 10; i++)
+        {
+            tasks[i] = Task.Run(() =>
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    StaticServer.AddToCount(1);
+                    Console.WriteLine("Write +1");
+                    Thread.Sleep(100);
+                }
+            });
+        }
+
+        Task.WaitAll(tasks);
+        Console.WriteLine($"{StaticServer.GetCount()}");
+    }
+}
---
+++ Server/Server/StaticServer.cs
@@ -0,0 +1,35 @@
+﻿namespace Server;
+
+public static class StaticServer
+{
+    private static int count = 0;
+
+    private static readonly ReaderWriterLockSlim _lock =
+        new ReaderWriterLockSlim();
+
+    public static int GetCount()
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            return count;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+
+    public static void AddToCount(int value)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            count += value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+}
